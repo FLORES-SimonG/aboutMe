1607e17bf5ce6b8e56e3c431c0b16f4adf6e26f8
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-01-30 10:23:14 +0100

    Actualización
    Modifiqué algunos textos y clases en HTML.
    He adicionado JavaScript, donde cumple las principales funcionalidades.
    Restaría en hacer los estilos finales para cuando se adicionan las actividades.

---
+++ scripts/index.js
@@ -0,0 +1,46 @@
+//! DEFINO QUÉ LUGAR QUIERO SELECCIONAR
+const formulario = document.querySelector('.form');
+//!BUSCO DENTRO DEL FORMULARIO Y SELECCIONO LO QUE QUIERO QUE LEA
+const titulo = formulario.querySelector('.nombreJS'); 
+const comentario = formulario.querySelector('.descripcionJS'); 
+const url = formulario.querySelector('.urlJS');
+
+//!LUGAR DONDE ENVIO
+const listaHobbies = document.querySelector('#envioActividad');
+
+
+//!MI FUNCIÓN QUE SE ACTIVA
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  render();
+});
+
+//!MI FUNCIÓN PRINCIPAL CUANDO SE ACTIVA LA ANTERIOR
+function render() {
+    //! CREO MIS ELEMENTOS
+  const lista = document.createElement('li');
+  let tituloRender = document.createElement('h4');
+  let imagenRender = document.createElement('img');
+  let parrafoRender = document.createElement('p');
+
+  //!LE ASIGNO VALOR A LOS MISMOS
+  tituloRender.textContent = titulo.value;
+  imagenRender.src = url.value;
+  imagenRender.setAttribute('alt', 'Imagen');
+  parrafoRender.textContent = comentario.value;
+
+  //!LE CREO HIJOS A ESOS ELEMENTOS
+  lista.appendChild(tituloRender);
+  lista.appendChild(imagenRender);
+  lista.appendChild(parrafoRender);
+
+  //! LE ADICIONO UNA ETIQUETA
+  lista.classList.toggle('renderLi');
+    //! A MI LISTA PRINCIPAL, LE ADICIONO TODO LO ANTERIOR
+  listaHobbies.appendChild(lista);
+
+  //!RESETEO VALORES
+  titulo.value = '';
+  comentario.value = '';
+  url.value = '';
+}
